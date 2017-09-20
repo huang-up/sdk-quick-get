ae51c3700996391bef109eb1d1472057e46764d5
huang-up <1101035657@qq.com>
2017-09-20 18:53:55 +0800

    简单工厂模式+策略模式

---
+++ src/test/java/design/chapter1/simplefactory/OperationFactory.java
@@ -0,0 +1,27 @@
+package design.chapter1.simplefactory;
+
+/**
+ * Created by admin on 2017/9/19.
+ */
+public class OperationFactory {
+    public static Operation createOperation(String operate) {
+        Operation operation = null;
+        switch (operate) {
+            case "+":
+                operation = new OperationAdd();
+                break;
+            case "-":
+                operation = new OperationSub();
+                break;
+            case "*":
+                operation = new OperationMul();
+                break;
+            case "/":
+                operation = new OperationDiv();
+                break;
+            default:
+                throw new RuntimeException("不支持的运算符。");
+        }
+        return  operation;
+    }
+}
---
+++ src/test/java/design/chapter1/simplefactory/OperationSub.java
@@ -0,0 +1,13 @@
+package design.chapter1.simplefactory;
+
+/**
+ * Created by admin on 2017/9/19.
+ */
+public class OperationSub extends Operation {
+    @Override
+    public double getResult() {
+        double result = 0;
+        result = numA - numB;
+        return result;
+    }
+}
