713f04a552873edfa3809dd14633410e4152d8fd
huang-up <1101035657@qq.com>
2017-09-20 18:55:11 +0800

    装饰模式

---
+++ src/test/java/design/chapter6/decoration/AppClient.java
@@ -0,0 +1,15 @@
+package design.chapter6.decoration;
+
+/**
+ * Created by admin on 2017/9/19.
+ */
+public class AppClient {
+    public static void main(String[] args) {
+        Person person = new Person("小菜");
+        TShirts tShirts = new TShirts();
+        tShirts.decorate(person);
+        BigTrouser bigTrouser = new BigTrouser();
+        bigTrouser.decorate(tShirts);
+        bigTrouser.show();
+    }
+}
---
+++ src/test/java/design/chapter6/decoration/Finery.java
@@ -0,0 +1,22 @@
+package design.chapter6.decoration;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by admin on 2017/9/19.
+ */
+public abstract class Finery extends Person {
+    private static final Logger logger = Logger.getLogger(Finery.class);
+    protected Person person;
+    public Finery() {}
+    public void decorate(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public void show() {
+        if (person != null) {
+            person.show();
+        }
+    }
+}
