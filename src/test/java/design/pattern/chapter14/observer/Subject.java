66fd5ddea05bdf4be642cf83751a5ff5d30698ed
huang-up <1101035657@qq.com>
2017-10-08 19:04:26 +0800

    观察者模式

---
+++ src/test/java/design/pattern/chapter14/observer/Subject.java
@@ -0,0 +1,19 @@
+package design.pattern.chapter14.observer;
+
+/**
+ * Created by h on 2017/10/8.
+ */
+public abstract class Subject {
+    private String subjectState;
+    public abstract void attach(Observer observer);
+    public abstract void detach(Observer observer);
+    public abstract void notifyObservers();
+
+    public String getSubjectState() {
+        return subjectState;
+    }
+
+    public void setSubjectState(String subjectState) {
+        this.subjectState = subjectState;
+    }
+}
