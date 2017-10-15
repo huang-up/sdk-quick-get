6f396b748ba568bfbb8939492343a36d7e7798af
huang-up <1101035657@qq.com>
2017-10-15 17:33:32 +0800

    适配器模式

---
+++ src/test/java/design/pattern/chapter17/adapter/Forwards.java
@@ -0,0 +1,21 @@
+package design.pattern.chapter17.adapter;
+
+/**
+ * Created by h on 2017/10/15.
+ */
+public class Forwards extends Player {
+
+    public Forwards(String name) {
+        super.name = name;
+    }
+
+    @Override
+    public void attack() {
+        super.logger.info(String.format("前锋：%s 进攻", super.name));
+    }
+
+    @Override
+    public void defence() {
+        super.logger.info(String.format("前锋：%s 防守", super.name));
+    }
+}
