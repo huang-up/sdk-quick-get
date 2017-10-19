9f7cb0b5f0d3df8bbd5e3fef9f0c8e8c18eaf082
huang-up <1101035657@qq.com>
2017-10-19 21:29:39 +0800

    备忘录模式(Memento)

---
+++ src/test/java/design/pattern/chapter18/memento/Caretaker.java
@@ -0,0 +1,16 @@
+package design.pattern.chapter18.memento;
+
+/**
+ * Created by h on 2017/10/19.
+ */
+public class Caretaker {
+    private Memento memento;
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
