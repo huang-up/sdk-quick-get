b42fcb2e14dea7bebd072e40e7be608ca213ac3a
huang-up <1101035657@qq.com>
2017-10-23 20:22:25 +0800

    职责链模式(Chain of Responsibility)

---
+++ src/test/java/design/pattern/chapter24/chainofresponsibility/example/Majordomo.java
@@ -0,0 +1,19 @@
+package design.pattern.chapter24.chainofresponsibility.example;
+
+/**
+ * Created by h on 2017/10/23.
+ */
+public class Majordomo extends Manager {
+    public Majordomo(String name) {
+        super.name = name;
+    }
+    @Override
+    public void requestApplication(Request request) {
+        if ("请假".equals(request.getType()) && request.getNumber() <= 5) {
+            super.logger.info(String.format("%s: %s 数量 %d 被批准",
+                    this.getClass().getName(), request.getContent(), request.getNumber()));
+        } else if (super.superior != null) {
+            super.superior.requestApplication(request);
+        }
+    }
+}
---
+++ src/test/java/design/pattern/chapter24/chainofresponsibility/example/Request.java
@@ -0,0 +1,43 @@
+package design.pattern.chapter24.chainofresponsibility.example;
+
+/**
+ * Created by h on 2017/10/23.
+ */
+public class Request {
+    private String name;
+    private String content;
+    private String type;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
