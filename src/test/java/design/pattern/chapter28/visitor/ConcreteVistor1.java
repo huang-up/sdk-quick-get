4ff5d261c0efce3a221e295815309b3fa1be3dc1
huang-up <1101035657@qq.com>
2017-10-28 22:56:41 +0800

    访问者模式(Vistor)

---
+++ src/test/java/design/pattern/chapter28/visitor/ConcreteVistor1.java
@@ -0,0 +1,16 @@
+package design.pattern.chapter28.visitor;
+
+/**
+ * Created by h on 2017/10/28.
+ */
+public class ConcreteVistor1 extends Vistor {
+    @Override
+    public void visitConcreteElementA(ConcreteElementA concreteElementA) {
+        super.logger.info(String.format("%s被%s访问", concreteElementA.getClass().getName(), this.getClass().getName()));
+    }
+
+    @Override
+    public void visitConcreteElementB(ConcreteElementB concreteElementB) {
+        super.logger.info(String.format("%s被%s访问", concreteElementB.getClass().getName(), this.getClass().getName()));
+    }
+}
