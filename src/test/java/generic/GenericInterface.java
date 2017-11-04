6dc29d951ca63ebe2b8c979890ebbc82009bcb61
huang-up <1101035657@qq.com>
2017-11-04 22:26:21 +0800

    泛型(非协变性),数组(协变性)

---
+++ src/test/java/generic/GenericInterface.java
@@ -0,0 +1,21 @@
+package generic;
+
+import generic.array.Shape;
+
+/**
+ * Created by h on 2017/11/2.
+ */
+
+public interface GenericInterface {
+    /**
+     * 泛型方法
+     */
+    <AnyType> int compareTo(AnyType anyType);
+}
+
+/**
+ * 泛型接口
+ */
+interface Generic<AnyType> {
+    int compareTo(AnyType anyType);
+}
---
+++ src/test/java/generic/array/Rectangle.java
@@ -0,0 +1,10 @@
+package generic.array;
+
+/**
+ * Created by h on 2017/11/2.
+ */
+public class Rectangle extends Shape {
+    public Rectangle(float radius) {
+        super.radius = radius;
+    }
+}
