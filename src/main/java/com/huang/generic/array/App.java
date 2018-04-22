e34c9014e091c11f74e7f828ef030a600634dc36
huang-up <1101035657@qq.com>
2018-04-22 18:52:16 +0800

    更新泛型相关备注和测试

--- src/main/java/com/huang/generic/array/App.java
+++ src/main/java/com/huang/generic/array/App.java
@@ -10,28 +10,30 @@ import java.lang.reflect.Method;
  */
 public class App {
     public static void test(Person[] arr) {
-        arr[0].run();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].run();
+        }
     }
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        Person[] arr = new Employee[2];
+        Person[] arr = new Person[2];
         arr[0] = new Employee();
-//        arr[0] = new Student();   java.lang.ArrayStoreException
+        arr[1] = new Student();   // java.lang.ArrayStoreException
         test(arr);
 
         GenericClass<Student> s = new GenericClass<Student>();
         s.write(new Student());
-//        GenericClass<? extends Person> gc = s;
-//        gc.read().run();
-        GenericClass<?> gc = s;
-        Class p = gc.read().getClass();
-        Method[] ms = p.getDeclaredMethods();
-        Method method = null;
-        for (Method m:ms) {
-            if (m.getName().equals("run")) {
-                method = m;
-                break;
-            }
-        }
-        method.invoke(gc.read());
+        GenericClass<? extends Person> gc = s;
+        gc.read().run();
+//        GenericClass<?> gc = s;
+//        Class p = gc.read().getClass();
+//        Method[] ms = p.getDeclaredMethods();
+//        Method method = null;
+//        for (Method m:ms) {
+//            if (m.getName().equals("run")) {
+//                method = m;
+//                break;
+//            }
+//        }
+//        method.invoke(gc.read());
     }
 }
